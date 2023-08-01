fde1dbc7ee905b041cb3194cdd9a87bed930af47
mussttfa <makin9948qgmail.com>
2023-08-01 18:18:05 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+//console.log('merhaba dünya')
+
+const menuBtn=document.querySelector('.navbar-menu-btn')
+
+//console.log(menuBtn)
+
+const menuLinks=document.querySelector('.navbar_links')
+
+//console.log(menuLinks)
+
+/*menuBtn.addEventListener('click',()=>{
+    menuLinks.classList.toggle('mobile-menu')
+})*/
+
+
+menuBtn.addEventListener('click',()=>{
+    menuLinks.classList.toggle('mobile-menu')
+})
